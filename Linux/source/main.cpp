eda2253206cda71d5e4eb5c822f1f9f6f3a8a0b6
DROOPER <ciumbalone@gmail.com>
2018-12-29 17:25:01 -0500

    Plaintext Database Integration

--- Linux/source/main.cpp
+++ Linux/source/main.cpp
@@ -1,9 +1,11 @@
-
+#include <string>
 #include <iostream>
 #include <fstream>
+#include <ctype.h>
 #include <unistd.h>
 using namespace std;
-int j=0,id,pin,sum;
+int j=1,id,pin,sum;
+
 struct accounts
 {
 int id;
@@ -31,6 +33,8 @@ cout<<"6.Exit"<<endl<<endl;
 cout<<"Choose an option:";
 }
 
+
+
 void auth()
 {
 cout<<"Enter account ID:";
@@ -51,13 +55,19 @@ void create()
 {
 banner();	
 account[j].id=j;
+string str=to_string(j);
+ofstream output(str.c_str());
+output<<j<<endl;
 account[j].balance=0;
 cout<<"Enter first name:";
 cin>>account[j].firstname;
+output<<account[j].firstname<<endl;
 cout<<"Enter last name:";
 cin>>account[j].lastname;
+output<<account[j].lastname<<endl;
 cout<<"Choose a PIN:";
 cin>>account[j].pin;
+output<<account[j].pin<<endl;
 cout<<endl;
 cout<<"Your account ID is ----> "<<j<<" <---- (Please note it somewhere)"<<endl;
 j++;
@@ -152,12 +162,34 @@ else {cout<<"Insufficient funds!";usleep(500000);}
 else wrongPIN();
 }
 
+void savedaccountscheck()
+{
+system("ls | sort -nr > asd.txt");
+ifstream v("asd.txt");
+v>>j;
 
+}
 
 
 
 int main()
 {
+	savedaccountscheck();
+
+	for(int y=0;y<=j;y++)
+{
+
+string a=to_string(y);
+ifstream input(a.c_str());
+input>>account[y].id;
+input>>account[y].firstname;
+input>>account[y].lastname;
+input>>account[y].pin;
+input>>account[y].balance;
+
+
+}
+j++;
 bool continuity=true;
 while(continuity==1)
 {
@@ -166,7 +198,7 @@ int n;
 message();
 cin>>n;
 switch(n) {
-case 1 : create();
+case 1 : create(); 
 break;
 case 2 : details();
 break;
@@ -176,7 +208,10 @@ case 4 : withdraw();
 break;
 case 5 : transfer();
 break;
-case 6 : continuity=false;
+case 6 : 
+for(int y=0;y<j;y++)
+	{string yy=to_string(y);string accountbalance=to_string(account[y].balance);system(("echo " + accountbalance + ">>" + yy).c_str());}
+continuity=false;
 break;
 }
 
